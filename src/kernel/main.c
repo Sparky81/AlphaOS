f99859555d24d338fa8430d1bcca973b04d4157e
Your Name <Sparky@ZeroNET.us>
2011-03-05 21:52:01 -0500

    Added kernel files but they're not operational yet.

---
+++ src/kernel/main.c
@@ -0,0 +1,9 @@
+/**************************************
+ *           AlphaOS Kernel           *
+ *	      Version 0.1             *
+ **************************************
+ *        Copyright (c) 2011          *
+ * Robert Schofield and Matthew Carey *
+ *       All rights reserved.         *
+ *************************************/
+
---
+++ src/kernel/start.asm
@@ -0,0 +1,6 @@
+; Kernel Start
+; Version 0.1
+; ------------------------------------------------------
+; Copyright (c) 2011, Robert Schofield and Matthew Carey
+; All rights reserved. 
+

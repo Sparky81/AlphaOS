bc55ada168cb8f06aa9b3fc1b8ff0acada5cc409
Robert Schofield <Sparky@ZeroNET.us>
2011-03-19 20:02:48 -0400

    Kernel Additions

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,9 +1,17 @@
-#include "Video.h"
+/*********************************************************
+  AlphaOS Kernel - kernel.cpp
+  Version 0.1
+  Copyright (c) 2011, Robert Schofield and Matthew Carey
+**********************************************************/
 
+// #include "include/keyboard.h"
+// #include "include/console.h"
+// #include "include/memory.h"
+#include "includes/Video.h"
 
 int main(void)
 {
     Video vid;
-  vid.clear();
-  vid.write("  >AlphaOS Kernel 0.0.1 (C) 2011 Robert Schofield and Matthew Carey<");
+    vid.clear();
+    vid.write("AlphaOS Kernel\nVersion 0.1\nCopyright (c) 2011, Robert Schofield and Matthew Carey\n\n");
 }

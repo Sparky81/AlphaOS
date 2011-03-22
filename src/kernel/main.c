2cf1b941ded2d72c931326cb271a605965df3fd6
Robert J. Schofield <robert@schofie.us>
2011-03-22 17:13:26 -0400

    Added a CPUID (not made by us, will be redone later) that is FULL of compiler warnings. Just ignore them.
    	new file:   src/kernel/cpuid.c
    	deleted:    src/kernel/includes/cpuid.h
    	deleted:    src/kernel/includes/cpuid.h~
    	deleted:    src/kernel/includes/video.h~
    
    	deleted:   src/kernel/includes/cpuid.h
    	deleted:   src/kernel/includes/cpuid.h~
    	deleted:   src/kernel/includes/video.h~

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,10 +5,12 @@
  */
 
 #include "includes/video.h"
+#include "cpuid.c"
 
 int main(struct multiboot *mboot_ptr)
 {
     clear();
-    printf("AlphaOS Kernel\nVersion 1.0\nCopyright (c) 2011, Robert Schofield and Matthew Carey");
+    printf("AlphaOS Kernel\nVersion 1.0\nCopyright (c) 2011, Robert Schofield and Matthew Carey\n\n");
+    detect_cpu();
     return 0;
 }

e837d50b472281a2bc72b70b6ecbcc25a30d72b2
Robert Schofield <rjschofield81@gmail.com>
2011-12-31 01:13:57 -0500

    Removed the indentation from the NOTICE when the kernel first starts.

--- src/main.c
+++ src/main.c
@@ -20,6 +20,6 @@ int show_intro()
   print("AlphaOS Kernel - Version 1.0\n");
   print("Copyright (c) 2011, Robert Schofield and Matthew Carey\n");
   print("All rights reserved.\n");
-  print("\n\n *** NOTICE ***\n");
-  print(" The Kernel currently does not have working keyboard handling or a console.\n");
+  print("\n\n*** NOTICE ***\n");
+  print("The Kernel currently does not have working keyboard handling or a console.\n");
 }

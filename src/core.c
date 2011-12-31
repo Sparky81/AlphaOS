d3d4f1e16b7ee8a3ae8c5736ca0614c863ccc5c3
Matt Carey <matthew.b.carey@gmail.com>
2011-12-30 23:47:28 -0500

    That's right, I did something. I created memcpy, memset, strcmp, strcpy, strcat, strlen, and make a technical correction to inb, outb, and inw. I basically rewrote the whole file. Does it all work? Who knows. All I know is that is somewhat close to correct and that's good enough for me, for now at least.

--- src/core.c
+++ src/core.c
@@ -9,7 +9,7 @@
 // Write a byte out to the specified port.
 void outb(u16int port, u8int value)
 {
-    asm volatile ("outb %1, %0" : : "dN" (port), "a" (value));
+    __asm__ __volatile__ ("outb %1, %0" : : "dN" (port), "a" (value));
 }
 
 u8int inb(u16int port)
@@ -29,32 +29,65 @@ u16int inw(u16int port)
 // Copy len bytes from src to dest.
 void memcpy(u8int *dest, const u8int *src, u32int len)
 {
-    // TODO: implement this yourself!
-}
+   const char *sp = (const char *)src; 
+   /* sp stands for source pointer and the (const char *) is a cast */ 
+   char *dp = (char *)dest; /* destination pointer, cast */
+   for( ; len != 0; len--) *dp++ = *sp++;
+   return dest;
+} 
+
 
 // Write len copies of val into dest.
 void memset(u8int *dest, u8int val, u32int len)
 {
-    // TODO: implement this yourself!
+    const char *dp = (char *)dest;
+    for( ; len != 0; len--) *dp++ = val;
+    return dest;
 }
 
 // Compare two strings. Should return -1 if 
 // str1 < str2, 0 if they are equal or 1 otherwise.
 int strcmp(char *str1, char *str2)
 {
-    // TODO: implement this yourself!
+    int len[2] = {0, 0};
+    int i = 0;
+    for( ; *str[i]; i++);
+    len[0] = i;
+    for(i = 0 ; *str[i]; i++);
+    len[1] = i;
+    if(len[0] < len[1])
+    {
+        return(-1);
+    }
+    else
+    {
+        if(len[0] == len[1])
+        {
+            return(0);
+        }
+        else
+        {
+            return(1);
+        } 
+    }
 }
 
 // Copy the NULL-terminated string src into dest, and
 // return dest.
 char *strcpy(char *dest, const char *src)
 {
-    // TODO: implement this yourself!
+    while(*src)
+    {
+       *dest++ = *src++;
+    }
+    return dest;
 }
 
 // Concatenate the NULL-terminated string src onto
 // the end of dest, and return dest.
 char *strcat(char *dest, const char *src)
 {
-    // TODO: implement this yourself!
+    for( ; *dest; *dest++);
+    for( ; *src; *dest++ = *src++);
+    return dest;
 }

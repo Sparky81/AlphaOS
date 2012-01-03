1815225bc7c3e0df9ef95bdc3b5a7de3f047b61a
Matt Carey <matthew.b.carey@gmail.com>
2012-01-03 12:58:30 -0500

    Added name param to irq_install_handler()

--- src/include/irq.h
+++ src/include/irq.h
@@ -6,7 +6,7 @@
 #ifndef IRQ_H
 #define IRQ_H
 
-void irq_install_handler(int irq, void (*handler)(struct regs *r));
+void irq_install_handler(int irq, void (*handler)(struct regs *r), char *handler_name);
 void irq_uninstall_handler(int irq);
 void irq_remap(void);
 void irq_install(void);

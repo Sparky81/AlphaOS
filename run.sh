1729b997b96dfc1b2ba677ef9bb4a5e567c5a205
Robert Schofield <rjschofield81@gmail.com>
2012-01-30 19:36:02 -0500

    SOME IDIOT BORKED THE RUN.SH FILE. It's fixed now.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f build/Kernel.bin ];
 then
-   qemu-system-i386 -kernel build/Kernel.bin
+   qemu -kernel build/Kernel.bin
 else
    make && qemu-system-i386 -kernel build/Kernel.bin
 fi 
